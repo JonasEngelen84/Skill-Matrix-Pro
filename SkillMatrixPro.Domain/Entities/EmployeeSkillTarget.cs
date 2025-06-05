a35c890e34d7e9389dce6bdc8d444054b69f7fd7
Jonas Engelen <jonasEngelen@hotmail.de>
2025-06-05 17:12:54 +0200

    EmployeeSkillTarget erstellt

---
+++ SkillMatrixPro.Domain/Entities/EmployeeSkillTarget.cs
@@ -0,0 +1,16 @@
+﻿namespace SkillMatrixPro.Domain.Entities
+{
+    public class EmployeeSkillTarget
+    {
+        public Guid Id { get; set; }
+
+        public Guid EmployeeId { get; set; }
+        public Employee Employee { get; set; } = null!;
+
+        public Guid SkillId { get; set; }
+        public Skill Skill { get; set; } = null!;
+
+        public int TargetLevel { get; set; }
+        public DateTime TargetDate { get; set; }
+    }
+}
