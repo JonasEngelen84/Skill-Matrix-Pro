c90fbba18ea416f1c9da89d95eb414f82aa0d826
Jonas Engelen <jonasEngelen@hotmail.de>
2025-06-23 00:20:50 +0200

    EmployeeSkillTargetTests & SkillLevelTests erzeugt

---
+++ SkillMatrixPro.Tests/EmployeeSkillTargetTests.cs
@@ -0,0 +1,48 @@
+﻿using SkillMatrixPro.Domain.Entities;
+using SkillMatrixPro.Domain.Value_Objects;
+
+namespace SkillMatrixPro.Tests
+{
+    public class EmployeeSkillTargetTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(6)]
+        public void Constructor_InvalidTargetLevel_ThrowsArgumentOutOfRangeException(SkillLevel invalidLevel)
+        {
+            var employee = Guid.NewGuid();
+            var skill = Guid.NewGuid();
+            var futureDate = DateTime.UtcNow.AddDays(1);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new EmployeeSkillTarget(employee, skill, invalidLevel, futureDate));
+        }
+
+        [Fact]
+        public void Constructor_TargetDateInPast_ThrowsArgumentException()
+        {
+            var employee = Guid.NewGuid();
+            var skill = Guid.NewGuid();
+            var pastDate = DateTime.UtcNow.AddDays(-1);
+
+            Assert.Throws<ArgumentException>(() =>
+                new EmployeeSkillTarget(employee, skill, 3, pastDate));
+        }
+
+        [Fact]
+        public void Constructor_ValidParameters_CreatesObjectCorrectly()
+        {
+            var employee = Guid.NewGuid();
+            var skill = Guid.NewGuid();
+            var targetLevel = 4;
+            var targetDate = DateTime.UtcNow.AddDays(10);
+
+            var target = new EmployeeSkillTarget(employee, skill, targetLevel, targetDate);
+
+            Assert.Equal(employee, target.EmployeeId);
+            Assert.Equal(skill, target.SkillId);
+            Assert.Equal(targetLevel, target.TargetLevel);
+            Assert.Equal(targetDate.Date, target.TargetDate);
+        }
+    }
+}
---
+++ SkillMatrixPro.Tests/SkillLevelTests.cs
@@ -0,0 +1,21 @@
+﻿using SkillMatrixPro.Domain.Value_Objects;
+
+namespace SkillMatrixPro.Tests;
+
+public class SkillLevelTests
+{
+    [Fact]
+    public void Constructor_ValidValue_SetsValue()
+    {
+        var level = new SkillLevel(3);
+        Assert.Equal(3, level.Value);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(6)]
+    public void Constructor_InvalidValue_ThrowsException(int invalidValue)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new SkillLevel(invalidValue));
+    }
+}
